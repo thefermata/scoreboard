777c7f7e4fcddcc37f9e274e5a07ad1438ad53bb
khm <the_fermata@naver.com>
2019-02-23 18:13:35 +0900

    component 분리

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import {Player} from "./components/Player";
 class App extends React.Component {
   state = {
     players: [
-      {name: 'Aa', id: 1},
-      {name: 'Bb', id: 2},
-      {name: 'c', id: 3},
-      {name: 'd', id: 4},
+      {name: 'Aa', id: 1, score: 0,},
+      {name: 'Bb', id: 2, score: 0,},
+      {name: 'c', id: 3, score: 0,},
+      {name: 'd', id: 4, score: 0,},
     ],
   };
   handleRemovePlayer = (id) => {
@@ -17,12 +17,32 @@ class App extends React.Component {
       players: prevState.players.filter(player => player.id !== id)
     }));
   };
+
+  handleChangeScore = (idx, delta) => {
+console.log(idx, delta)
+    const players = this.state.players.map((player, i)=>{
+      if (idx == i) {
+        player.score += delta;
+      }
+      return player;
+    });
+
+    this.setState({players});
+  }
+
   render() {
     return (
       <div className="scoreboard">
         <Header title="My scoreboard" totalPlayers={ 1+10 }/>
         {
-          this.state.players.map(play => <Player name={play.name} score={play.score} id={play.id} key={play.id} removePlayer={this.handleRemovePlayer} />)
+          this.state.players.map((play, index) => <Player
+            name={play.name}
+            score={play.score}
+            id={play.id}
+            key={play.id}
+            index={index}
+            removePlayer={this.handleRemovePlayer}
+            changeScore={this.handleChangeScore} />)
         }
       </div>
     )
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,25 +3,14 @@ import React from 'react';
 export class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      players:1,
-      score: 0
-    }
-  }
-  incrementScore = ()=> {
-    this.setState(prevState => ({ score: prevState.score + 1 }));
-  };
-
-  decrementScrore = ()=> {
-    this.setState(prevState => ({ score: prevState.score - 1 }));
   }
 
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement" onClick={this.decrementScrore}>-</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={this.incrementScore}>+</button>
+        <button className="counter-action decrement" onClick={()=>this.props.changeScore(this.props.index,-1)}>-</button>
+        <span className="counter-score">{this.props.score}</span>
+        <button className="counter-action increment" onClick={()=>this.props.changeScore(this.props.index,1)}>+</button>
       </div>
     )
   }
